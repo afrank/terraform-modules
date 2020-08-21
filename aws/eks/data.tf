53436ed53258ae33d3efa3d71b4b40e739a560ea
Alberto del Barrio <adelbarrio@mozilla.com>
2020-08-21 16:35:24 +0200

    [aws][eks] Get a clean cluster deletion state

--- aws/eks/data.tf
+++ aws/eks/data.tf
@@ -1,9 +1,11 @@
 data "aws_caller_identity" "current" {}
 
 data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_id
+  name       = module.eks.cluster_id
+  depends_on = [module.eks]
 }
 
 data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_id
+  name       = module.eks.cluster_id
+  depends_on = [module.eks]
 }
